cdc9e8b53397058b9827162f0052c9b06aaf37ae
Shiyao <biansyao@gmail.com>
2021-01-25 23:59:44 +0800

    lecture5 first order methods

--- first_order_algorithm/main.py
+++ first_order_algorithm/main.py
@@ -5,12 +5,12 @@ x = np.array([10.0, 10.0])
 # x = np.array([0.0, 0.0])
 eps, sigma, beta = 1e-4, 0.5, 0.5
 
-res, descent_step, line_search_step, x_list, y_list = steepest_descent(x, 0.0001, 0.5, 0.5)
+res, descent_step, line_search_step, x_list, y_list = steepest_descent(x, eps, sigma, beta)
 x_arr = np.array(x_list)
 x_range = [8, 15]
 y_range = [2, 12]
 levels = np.arange(0, 20, 2) ** 2
-title = 'Start from [10, 10], armijo takes 35 steps to converge within 1e-4'
+title = f'Start from [10, 10], armijo takes {descent_step} steps to converge within {eps}'
 contour_plot(0.5, x_range, y_range, x_arr, levels, title)
 
 y_arr = np.array(y_list)
